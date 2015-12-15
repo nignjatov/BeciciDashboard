df71901975adccb9cdc822aeec53e9214bc8d9c9
Nemanja Ignjatov <nemanja.ignjatov@rt-rk.com>
2015-12-15 05:18:24 +0100

    Contact and social pages

--- src/js/controllers/contactCtrl.js
+++ src/js/controllers/contactCtrl.js
@@ -1,7 +1,27 @@
-
 angular.module('RDash')
   .controller('contactCtrl', ['$scope', '$cookieStore', contactCtrl]);
 
 function contactCtrl($scope) {
+
+  $scope.contact = {
+    latitude: 42.28295,
+    longitude: 18.87260
+  };
+
+  $scope.saveContactData = function () {
+    console.log($scope.contact);
+  }
+  function initialize() {
+    var mapProp = {
+      center: new google.maps.LatLng(42.28295, 18.87260),
+      zoom: 10,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map= new google.maps.Map(document.getElementById("googleMap"), mapProp);
+  }
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+
+
 
 }
--- src/js/controllers/socialCtrl.js
+++ src/js/controllers/socialCtrl.js
@@ -1,7 +1,42 @@
-
 angular.module('RDash')
   .controller('socialCtrl', ['$scope', '$cookieStore', socialCtrl]);
 
 function socialCtrl($scope) {
 
+  $scope.addNetworkName  = "";
+  $scope.defaultNetworks = [
+    "Facebook",
+    "Twitter",
+    "YouTube"
+  ];
+  $scope.socials         = [
+    {
+      name: "Facebook",
+      active: true,
+      link: "www.facebook.com"
+    }
+  ];
+
+  $scope.addNetwork = function () {
+    console.log("ADD " + $scope.addNetworkName);
+    $scope.socials.push({
+      name: $scope.addNetworkName,
+      active: true,
+      link: "www." + $scope.addNetworkName + ".com"
+    });
+    filterNetworks();
+    console.log($scope.socials);
+  }
+
+  function filterNetworks() {
+    for (var i = 0; i < $scope.defaultNetworks.length; i++) {
+      for (var j = 0; j < $scope.socials.length; j++) {
+          if($scope.defaultNetworks[i] == $scope.socials[j].name){
+            $scope.defaultNetworks.splice(i,1);
+          }
+      }
+    }
+  }
+
+  filterNetworks();
 }
