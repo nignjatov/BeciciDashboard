6591f091419eacee4957b52884ca4b1877e3571b
Nemanja Ignjatov <nemanja.ignjatov@rt-rk.com>
2015-12-26 13:14:47 +0100

    Missing file

---
+++ src/js/app.js
@@ -0,0 +1,46 @@
+var RDashApp = angular.module('RDash', ['ui.router', 'ngCookies','flow','ui-notification','ui.bootstrap','ui.bootstrap.datetimepicker'])
+  .filter('html', function ($sce) {
+  return function (input) {
+    return $sce.trustAsHtml(input);
+  }
+  })
+  .filter('language', function ($sce) {
+    return function (input) {
+      if(input === 'en'){
+        return $sce.trustAsHtml('English');
+      } else if (input === 'rs'){
+        return $sce.trustAsHtml('Srpski');
+      }
+    }
+  })
+  .config(['flowFactoryProvider','$httpProvider', function (flowFactoryProvider,$httpProvider) {
+
+    $httpProvider.interceptors.push('myHttpInterceptor');
+
+    flowFactoryProvider.defaults = {
+      target: 'upload.php',
+      permanentErrors: [404, 500, 501],
+      maxChunkRetries: 1,
+      chunkRetryInterval: 5000,
+      simultaneousUploads: 4,
+      singleFile: true
+    };
+    flowFactoryProvider.on('catchAll', function (event) {
+      console.log('catchAll', arguments);
+    });
+    // Can be used with different implementations of Flow.js
+    // flowFactoryProvider.factory = fustyFlowFactory;
+  }]);
+
+RDashApp.run(function ($rootScope){
+
+  $rootScope.currency = "RSD";
+  $rootScope.dateFormat = 'medium';
+  $rootScope.defaultLang = 'en';
+  $rootScope.languages = [
+    'en',
+    'rs'
+  ]
+
+});
+
